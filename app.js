4f3330bf9ca8481a71c1adf6a02d49efb3b8a9d4
Skillenot <william32089@gmail.com>
2025-07-27 13:28:27 -0500

    TitleFix

--- app.js
+++ app.js
@@ -258,7 +258,7 @@ const FinanzasNube = () => {
         <div className="header-content">
           <div className="logo">
             <i className="fas fa-wallet"></i>
-            <span>Finanzas en la Nube</span>
+            <span>LokiFinance</span>
           </div>
           <div className="header-controls">
             <div className="select-wrapper">
